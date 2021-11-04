b284664cba5993f25d1c195749d9f543fb69b50d
pierre joel <pierre_joel@live.fr>
2021-11-04 15:30:19 +0100

    fix tables

--- database/deploy/constraints.sql
+++ database/deploy/constraints.sql
@@ -1,9 +1,10 @@
 -- Deploy oparadise:constraints to pg
 
 BEGIN;
+DROP DOMAIN IF EXISTS fr_code,email,code_phone,code_password; 
 
-CREATE DOMAIN city_code AS INT CHECK(
-    VALUE ~ '^\d{5}$' -- cas général  \d correspond à [0-9] en SQL
+  CREATE DOMAIN fr_code as text CHECK (
+    VALUE ~ '^\d{5}$' 
     AND (
       VALUE ~ '^0[1-9]\d{3}$' -- codes qui commencent par 0 sauf 00
       OR VALUE ~ '^9[0-6]\d{3}$' -- codes qui commencent par 9 hors DOM et TOM
@@ -11,32 +12,34 @@ CREATE DOMAIN city_code AS INT CHECK(
       OR VALUE ~ '^9[78][12478]\d{2}$' -- codes DOM et TOM
     )
   );
-
-  ALTER TABLE
-  user_account
-ALTER COLUMN
-  city_code TYPE city_code;
-
-  CREATE DOMAIN email AS text CHECK (
+  
+ALTER TABLE user_account ALTER COLUMN city_code TYPE fr_code;
+ 
+   
+CREATE DOMAIN email AS text CHECK (
       VALUE ~ '^\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z$'
   );
 
-ALTER TABLE
-   user_account
-    ALTER COLUMN
-    email TYPE email;
 
-    CREATE DOMAIN number_phone AS INT CHECK (
+ALTER TABLE
+   user_account ALTER COLUMN email TYPE email;
+    
+    
+
+CREATE DOMAIN code_phone as text CHECK (
     VALUE ~ '^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$'
 );
 
-ALTER TABLE user_account ALTER COLUMN number_phone TYPE number_phone;
+ALTER TABLE user_account ALTER COLUMN number_phone TYPE code_phone; 
+
+ 
 
 
-CREATE DOMAIN password AS VARCHAR (255) CHECK(
+CREATE DOMAIN code_password AS VARCHAR (255) CHECK(
     VALUE ~ '^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
 );
-ALTER TABLE user_account ALTER COLUMN password TYPE password;
+
+ALTER TABLE user_account ALTER COLUMN password TYPE code_password;
 
 CREATE DOMAIN pint AS int CHECK(VALUE > 0);
 CREATE DOMAIN supint AS int CHECK(VALUE >= 9);
